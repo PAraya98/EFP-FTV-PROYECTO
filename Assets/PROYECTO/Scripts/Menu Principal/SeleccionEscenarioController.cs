64d6f81eb8417601faf191aa0aa027af3e7dc37c
PAraya98 <pedroaraya3098@gmail.com>
2022-12-15 07:05:56 -0300

    MENU PRINCIPAL ACTUALIZADO + TRANSICIÓN DE INICIO

--- Assets/PROYECTO/Scripts/Menu Principal/SeleccionEscenarioController.cs
+++ Assets/PROYECTO/Scripts/Menu Principal/SeleccionEscenarioController.cs
@@ -9,14 +9,14 @@ using UnityEngine.SceneManagement;
 public class SeleccionEscenarioController : MonoBehaviour
 {
     // Start is called before the first frame update
-    [BoxGroup("Valores Requeridos")]
+    [Header("Valores requeridos")]
     public GameObject escenario;
-    [BoxGroup("Valores Requeridos")]
     public string nombreEscenario;
-    [BoxGroup("Valores Requeridos")] [Scene]
+    public bool tieneIntro = true;
+    [Scene]
     public string escena;
 
-
+    private Animator animatorIntro;
     void Start()
     {
 
@@ -37,11 +37,14 @@ public class SeleccionEscenarioController : MonoBehaviour
             .Find("Image - Escenario")
             .GetComponent<RawImage>()
             .texture = RuntimePreviewGenerator.GenerateModelPreview(escenario.transform, 400, 400);
+        
+        animatorIntro = GameObject.Find("Panel -Intro").GetComponent<Animator>();
     }
 
     public void CargarEscena()
     {
-        StartCoroutine(LoadYourAsyncScene());
+        if (!tieneIntro) StartCoroutine(LoadYourAsyncScene());
+        else StartCoroutine(MostrarIntro());
     }
     IEnumerator LoadYourAsyncScene()
     {
@@ -50,6 +53,20 @@ public class SeleccionEscenarioController : MonoBehaviour
         // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
         // a sceneBuildIndex of 1 as shown in Build Settings.
 
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(escena);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+    IEnumerator MostrarIntro()
+    {
+        animatorIntro.Play("intro");
+        if (animatorIntro.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+            yield return new WaitForSeconds(animatorIntro.GetCurrentAnimatorStateInfo(0).length);
+        yield return new WaitForSeconds(animatorIntro.GetCurrentAnimatorStateInfo(0).length);
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(escena);
 
         // Wait until the asynchronous scene fully loads
