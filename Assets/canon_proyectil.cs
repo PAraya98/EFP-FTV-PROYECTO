a236ab5a91c4f85db34fb5fe0af5003a0f64683a
thecarrot912 <thecarrot911@gmail.com>
2022-12-15 07:32:26 -0300

    Se arreglo el canon nuevamente y se creo un nivel nuevo en la escena gabriel_dev_1 creo

--- Assets/canon_proyectil.cs
+++ Assets/canon_proyectil.cs
@@ -36,14 +36,14 @@ public class canon_proyectil : MonoBehaviour
             Mira_canon.transform.localPosition = position;
             Mira_canon.transform.Rotate(0, 0, 0, Space.Self);
 
-            velocidad_x = velocidad * -1;
+            velocidad_x = velocidad;
             velocidad_y = velocidad - 9.8f * Time.deltaTime;
             GameObject disparo = Instantiate(bala_proyectil_prefab, Mira_canon.position, Mira_canon.rotation);
             disparo.GetComponent<Rigidbody2D>().AddForce(new Vector2(velocidad_x, velocidad_y), ForceMode2D.Impulse);
         }
         else
         {
-            velocidad_x = velocidad;
+            velocidad_x = velocidad * - 1;
             velocidad_y = velocidad - 9.8f * Time.deltaTime;
             
             Vector3 position = new Vector3(-0.87f, 0.87f, 0);
